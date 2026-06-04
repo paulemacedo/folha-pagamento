9b6491dfbcc41c24eab5b573f3ca958734fc9dfe
Paule Macedo <paulemacedo@outlook.com>
2026-06-04 16:53:50 -0300

    first commit

---
+++ src/folhaPagamento/Funcionario.java
@@ -0,0 +1,5 @@
+package folhaPagamento;
+
+public class Funcionario {
+
+}
---
+++ src/folhaPagamento/Main.java
@@ -0,0 +1,10 @@
+package folhaPagamento;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
